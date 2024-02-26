d45d2fdbb2db41d580c5c538049436aaf78cfbd6
Issac <vovannghi20@gmail.com>
2024-02-26 23:52:20 +0700

    update

--- components/Name/index.tsx
+++ components/Name/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Name = () => {
   return (
-    <p className="text-4xl">
+    <p className="text-4xl text-center">
       ❤️
       <span className="text-4xl font-black text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
         <span className="[text-shadow:_2px_3px_2px_rgb(239_99_99)]">
